ea3111937424f2b7e82483e5e17da470e1359b7f
ErickMagallan <arqeemg@gmail.com>
2023-10-07 21:18:41 -0600

    collaborators e imagenes

---
+++ src/data/projectsPhotos.js
@@ -0,0 +1,130 @@
+
+import project1foto1 from '../assets/ilustracion/postales/1.JPG';
+import project1foto2 from '../assets/ilustracion/postales/2.JPG';
+import project1foto3 from '../assets/ilustracion/postales/3.JPG';
+import project1foto4 from '../assets/ilustracion/postales/4.JPG';
+import project1foto5 from '../assets/ilustracion/postales/5.JPG';
+import project1foto6 from '../assets/ilustracion/postales/6.jpeg';
+import project1foto7 from '../assets/ilustracion/postales/7.jpeg';
+import project1foto8 from '../assets/ilustracion/postales/8.jpeg';
+import project1foto9 from '../assets/ilustracion/postales/9.jpeg';
+import project1foto10 from '../assets/ilustracion/postales/10.jpeg';
+
+import project2foto1 from '../assets/ilustracion/mercancia/1.jpeg';
+import project2foto2 from '../assets/ilustracion/mercancia/2.jpeg';
+import project2foto3 from '../assets/ilustracion/mercancia/3.jpeg';
+import project2foto4 from '../assets/ilustracion/mercancia/4.jpeg';
+import project2foto5 from '../assets/ilustracion/mercancia/5.jpeg';
+import project2foto6 from '../assets/ilustracion/mercancia/6.jpeg';
+import project2foto7 from '../assets/ilustracion/mercancia/7.jpeg';
+import project2foto8 from '../assets/ilustracion/mercancia/8.jpeg';
+import project2foto9 from '../assets/ilustracion/mercancia/9.jpeg';
+import project2foto10 from '../assets/ilustracion/mercancia/10.jpeg';
+import project2foto11 from '../assets/ilustracion/mercancia/11.jpeg';
+import project2foto12 from '../assets/ilustracion/mercancia/12.jpeg';
+import project2foto13 from '../assets/ilustracion/mercancia/13.jpeg';
+import project2foto14 from '../assets/ilustracion/mercancia/14.jpeg';
+import project2foto15 from '../assets/ilustracion/mercancia/15.jpeg';
+import project2foto16 from '../assets/ilustracion/mercancia/16.jpeg';
+import project2foto17 from '../assets/ilustracion/mercancia/17.jpeg';
+import project2foto18 from '../assets/ilustracion/mercancia/18.jpeg';
+import project2foto19 from '../assets/ilustracion/mercancia/19.jpeg';
+import project2foto20 from '../assets/ilustracion/mercancia/20.jpeg';
+
+import project3foto1 from '../assets/ilustracion/menus/1.jpeg';
+import project3foto2 from '../assets/ilustracion/menus/2.jpeg';
+import project3foto3 from '../assets/ilustracion/menus/3.jpeg';
+import project3foto4 from '../assets/ilustracion/menus/4.jpeg';
+import project3foto5 from '../assets/ilustracion/menus/5.jpeg';
+import project3foto6 from '../assets/ilustracion/menus/6.jpeg';
+import project3foto7 from '../assets/ilustracion/menus/7.jpeg';
+import project3foto8 from '../assets/ilustracion/menus/8.jpeg';
+import project3foto9 from '../assets/ilustracion/menus/9.jpeg';
+
+import project4foto1 from '../assets/ilustracion/logotipo/1.jpeg';
+import project4foto2 from '../assets/ilustracion/logotipo/2.jpeg';
+import project4foto3 from '../assets/ilustracion/logotipo/3.jpeg';
+import project4foto4 from '../assets/ilustracion/logotipo/4.jpeg';
+import project4foto5 from '../assets/ilustracion/logotipo/5.jpeg';
+import project4foto6 from '../assets/ilustracion/logotipo/6.jpeg';
+
+import project5foto1 from '../assets/ilustracion/entrada_suchitlan/1.jpeg';
+import project5foto2 from '../assets/ilustracion/entrada_suchitlan/2.jpeg';
+import project5foto3 from '../assets/ilustracion/entrada_suchitlan/3.jpeg';
+
+import project6foto1 from '../assets/ilustracion/calendarios/1.jpeg';
+import project6foto2 from '../assets/ilustracion/calendarios/2.jpeg';
+import project6foto3 from '../assets/ilustracion/calendarios/3.jpeg';
+import project6foto4 from '../assets/ilustracion/calendarios/4.jpeg';
+import project6foto5 from '../assets/ilustracion/calendarios/5.jpeg';
+import project6foto6 from '../assets/ilustracion/calendarios/6.jpeg';
+import project6foto7 from '../assets/ilustracion/calendarios/7.jpeg';
+import project6foto8 from '../assets/ilustracion/calendarios/8.jpeg';
+
+import project7foto1 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/1.jpeg';
+import project7foto2 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/2.jpeg';
+import project7foto3 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/3.jpeg';
+import project7foto4 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/4.jpeg';
+import project7foto5 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/5.jpeg';
+import project7foto6 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/6.jpeg';
+import project7foto7 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/7.jpeg';
+import project7foto8 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/8.jpeg';
+import project7foto9 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/9.jpeg';
+import project7foto10 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/10.jpeg';
+import project7foto11 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/11.jpeg';
+import project7foto12 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/12.jpeg';
+import project7foto13 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/13.jpeg';
+import project7foto14 from '../assets/ilustracion/coleccion_de_sketchbooks_botanicum/14.jpeg';
+
+import project8foto1 from '../assets/ilustracion/proyecto_final_de_diplomado_el_lugar_de_las_flores/1.jpeg';
+import project8foto2 from '../assets/ilustracion/proyecto_final_de_diplomado_el_lugar_de_las_flores/2.jpeg';
+import project8foto3 from '../assets/ilustracion/proyecto_final_de_diplomado_el_lugar_de_las_flores/3.jpeg';
+import project8foto4 from '../assets/ilustracion/proyecto_final_de_diplomado_el_lugar_de_las_flores/4.jpeg';
+import project8foto5 from '../assets/ilustracion/proyecto_final_de_diplomado_el_lugar_de_las_flores/5.jpeg';
+import project8foto6 from '../assets/ilustracion/proyecto_final_de_diplomado_el_lugar_de_las_flores/6.jpeg';
+
+import project9foto1 from '../assets/ilustracion/tarjetas_de_proximidad/1.jpeg';
+import project9foto2 from '../assets/ilustracion/tarjetas_de_proximidad/2.jpeg';
+import project9foto3 from '../assets/ilustracion/tarjetas_de_proximidad/3.jpeg';
+
+import project10foto1 from '../assets/ilustracion/entrada_pablo_medita/1.jpeg';
+import project10foto2 from '../assets/ilustracion/entrada_pablo_medita/2.jpeg';
+import project10foto3 from '../assets/ilustracion/entrada_pablo_medita/3.jpeg';
+import project10foto4 from '../assets/ilustracion/entrada_pablo_medita/4.jpeg';
+import project10foto5 from '../assets/ilustracion/entrada_pablo_medita/5.jpeg';
+
+import project11foto1 from '../assets/ilustracion/ciudad/1.jpeg';
+import project11foto2 from '../assets/ilustracion/ciudad/2.jpeg';
+import project11foto3 from '../assets/ilustracion/ciudad/3.jpeg';
+import project11foto4 from '../assets/ilustracion/ciudad/4.jpeg';
+import project11foto5 from '../assets/ilustracion/ciudad/5.jpeg';
+import project11foto6 from '../assets/ilustracion/ciudad/6.jpeg';
+import project11foto7 from '../assets/ilustracion/ciudad/7.jpeg';
+import project11foto8 from '../assets/ilustracion/ciudad/8.jpeg';
+
+import project12foto1 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/1.jpeg';
+import project12foto2 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/2.jpeg';
+import project12foto3 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/3.jpeg';
+import project12foto4 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/4.jpeg';
+import project12foto5 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/5.png';
+import project12foto6 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/6.png';
+import project12foto7 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/7.png';
+import project12foto8 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/8.jpeg';
+import project12foto9 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/9.png';
+import project12foto10 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/10.png';
+import project12foto11 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/11.png';
+import project12foto12 from '../assets/ilustracion/imagen_de_identidad_la_ola_continua/12.png';
+
+export const projectsPhotos = {Postales : [project1foto1, project1foto2, project1foto3, project1foto4, project1foto5, project1foto6, project1foto7, project1foto8, project1foto9, project1foto10],
+                                Mercancía : [project2foto1, project2foto2, project2foto3, project2foto4, project2foto5, project2foto6, project2foto7, project2foto8, project2foto9, project2foto10, project2foto11, project2foto12, project2foto13, project2foto14, project2foto15, project2foto16, project2foto17, project2foto18, project2foto19, project2foto20],
+                                Menús : [project3foto1, project3foto2, project3foto3, project3foto4, project3foto5, project3foto6, project3foto7, project3foto8, project3foto9],
+                                Logotipo : [project4foto1, project4foto2, project4foto3, project4foto4, project4foto5, project4foto6],
+                                Entrada | Suchitlán : [project5foto1, project5foto2, project5foto3],
+                                Calendarios : [project6foto1, project6foto2, project6foto3, project6foto4, project6foto5, project6foto6, project6foto7, project6foto8],
+                                Colección de Sketchbooks | Botanicum : [project7foto1, project7foto3, project7foto3, project7foto4, project7foto5, project7foto6, project7foto7, project7foto8, project7foto9, project7foto10, project7foto11, project7foto12, project7foto13, project7foto14],
+                                Proyecto Final de Diplomado | El lugar de las flores : [project8foto1, project8foto2, project8foto3, project8foto4, project8foto5, project8foto6],
+                                Tarjetas de Proximidad : [project9foto1, project9foto2, project9foto3],
+                                Entrada | Pablo medita : [project10foto1, project10foto2, project10foto3, project10foto4, project10foto5],
+                                Ciudad : [project11foto1, project11foto2, project11foto3, project11foto4, project11foto5, project11foto6, project11foto7, project11foto8],
+                                Imagen de Identidad | La Ola Continúa : [project12foto1, project12foto2, project12foto3,project12foto4, project12foto5, project12foto6, project12foto7, project12foto8, project12foto9, project12foto10, project12foto11, project12foto12]
+                                }
