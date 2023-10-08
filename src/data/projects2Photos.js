72700c07cabfdf51fdc1087107e3492141fc754b
ErickMagallan <arqeemg@gmail.com>
2023-10-08 10:48:58 -0600

    import fotos editorial y mural

---
+++ src/data/projects2Photos.js
@@ -0,0 +1,39 @@
+import project1foto1 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/1.jpeg';
+import project1foto2 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/2.jpeg';
+import project1foto3 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/3.jpeg';
+import project1foto4 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/4.jpeg';
+import project1foto5 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/5.jpeg';
+import project1foto6 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/6.jpeg';
+import project1foto7 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/7.jpeg';
+import project1foto8 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/8.jpeg';
+import project1foto9 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/9.jpeg';
+import project1foto10 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/10.jpeg';
+import project1foto11 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/11.jpeg';
+import project1foto12 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/12.jpeg';
+import project1foto13 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/13.jpeg';
+import project1foto14 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/14.jpeg';
+import project1foto15 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/15.jpeg';
+import project1foto16 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/16.jpeg';
+import project1foto17 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/17.jpeg';
+import project1foto18 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/18.jpeg';
+import project1foto19 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/19.jpeg';
+import project1foto20 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/20.jpeg';
+import project1foto21 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/21.jpeg';
+import project1foto22 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/22.jpeg';
+import project1foto23 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/23.jpeg';
+import project1foto24 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/24.jpeg';
+import project1foto25 from '../assets/editorial/libro_de_poesia_infantil_canturia_arrullos_y_poemas/25.jpeg';
+
+import project2foto1 from '../assets/editorial/calendario_2023_cuerpo_de_agua/1.jpeg';
+import project2foto2 from '../assets/editorial/calendario_2023_cuerpo_de_agua/2.jpeg';
+import project2foto3 from '../assets/editorial/calendario_2023_cuerpo_de_agua/3.jpeg';
+import project2foto4 from '../assets/editorial/calendario_2023_cuerpo_de_agua/4.jpeg';
+
+import project3foto1 from '../assets/editorial/maria_sabina/1.jpeg';
+
+
+export const projectsPhotos = {
+    0 : [project1foto1, project1foto2, project1foto3, project1foto4, project1foto5, project1foto6, project1foto7, project1foto8, project1foto9, project1foto10, project1foto11, project1foto12, project1foto12, project1foto13, project1foto14, project1foto15, project1foto16, project1foto17, project1foto18, project1foto19, project1foto20, project1foto21, project1foto22, project1foto23, project1foto24, project1foto25],
+    1 : [project2foto1, project2foto2, project2foto3, project2foto4],
+    2 : [project3foto1],
+}
---
+++ src/data/projects3Photos.js
@@ -0,0 +1,31 @@
+import project1foto1 from '../assets/mural/casa_m47/1.jpeg';
+import project1foto2 from '../assets/mural/casa_m47/2.jpeg';
+import project1foto3 from '../assets/mural/casa_m47/3.jpeg';
+import project1foto4 from '../assets/mural/casa_m47/4.jpeg';
+import project1foto5 from '../assets/mural/casa_m47/5.jpeg';
+import project1foto6 from '../assets/mural/casa_m47/6.jpeg';
+import project1foto7 from '../assets/mural/casa_m47/7.jpeg';
+import project1foto8 from '../assets/mural/casa_m47/8.jpeg';
+
+import project2foto1 from '../assets/mural/desfile_de_luces/1.jpeg';
+import project2foto2 from '../assets/mural/desfile_de_luces/2.jpeg';
+import project2foto3 from '../assets/mural/desfile_de_luces/3.jpeg';
+import project2foto4 from '../assets/mural/desfile_de_luces/4.jpeg';
+import project2foto5 from '../assets/mural/desfile_de_luces/5.jpeg';
+import project2foto6 from '../assets/mural/desfile_de_luces/6.jpeg';
+import project2foto7 from '../assets/mural/desfile_de_luces/7.jpeg';
+import project2foto8 from '../assets/mural/desfile_de_luces/8.jpeg';
+import project2foto9 from '../assets/mural/desfile_de_luces/9.jpeg';
+import project2foto10 from '../assets/mural/desfile_de_luces/10.jpeg';
+
+import project3foto1 from '../assets/mural/verano/1.jpeg';
+import project3foto2 from '../assets/mural/verano/2.jpeg';
+import project3foto3 from '../assets/mural/verano/3.jpeg';
+import project3foto4 from '../assets/mural/verano/4.jpeg';
+
+
+export const projectsPhotos = {
+    0 : [project1foto1, project1foto2, project1foto3, project1foto4, project1foto5, project1foto6, project1foto7, project1foto8],
+    1 : [project2foto1, project2foto2, project2foto3, project2foto4, project2foto5, project2foto6, project2foto7, project2foto8, project2foto9, project2foto10],
+    2 : [project3foto1, project3foto2, project3foto3, project3foto4],
+}
